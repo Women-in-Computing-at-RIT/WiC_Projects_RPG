3ee705c62c149a475cfa590c2042007c67eee179
Quackson7 <jpw9845@rit.edu>
2024-09-19 19:26:28 -0400

    pushable chicken house

--- Player.gd
+++ Player.gd
@@ -4,10 +4,6 @@ const ACCELERATION = 600
 const MAX_SPEED = 100
 const FRICTION = 800
 
-# For Blocks
-const PUSH_SPEED = 20
-const BLOCK_MAX_VELOCITY = 50
-
 var velocity = Vector2.ZERO
 
 # Called every tic 
@@ -21,16 +17,5 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta) # accounts for frame rate
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) # accounts for frame rate
-	
-	if get_slide_count() > 0: # Handle block collisions
-		check_box_collision(velocity)
-		
-	velocity = move_and_slide(velocity) # auto handles delta 
 
-func check_box_collision(velocity: Vector2) -> void:
-	# print(velocity.x, velocity.y)
-	if abs(velocity.x) + abs(velocity.y) > BLOCK_MAX_VELOCITY:
-		return
-	var box = get_slide_collision(0).collider as Moveable
-	if box:
-		box.push(PUSH_SPEED * velocity)
+	velocity = move_and_slide(velocity) # auto handles delta 
