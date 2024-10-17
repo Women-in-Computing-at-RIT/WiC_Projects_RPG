47f5b623ac320f738cebbaf19e60b93a8a879c91
Exokem <ex010@live.com>
2024-10-17 19:20:34 -0400

    Link keyword properties, update prototype

--- Golisano/Keyword/Keyword.gd
+++ Golisano/Keyword/Keyword.gd
@@ -2,13 +2,18 @@ extends AxisAlignedBody2D
 
 export var labelText = ""
 export var labelColor = Color(0.0, 1.0, 0.0)
+export var backgroundColor = Color(0.0, 0.0, 0.0)
 
 onready var background = $ColorRect
 onready var label = $Label
 
 func _ready():
 	var scaled_size = collider_size * Properties.TILE_SIZE
+	
 	background._set_size(scaled_size)
+	background.set_frame_color(backgroundColor)
+	
+	label.set_text(labelText)
 	label._set_size(scaled_size / label.get_scale())
 	label.add_color_override("font_color", labelColor)
 	
