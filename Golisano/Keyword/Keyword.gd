70d1ebda01b99b2587a40a9027484ef09b4c07f6
Exokem <ex010@live.com>
2024-10-24 19:28:56 -0400

    Add keyword position reset

--- Golisano/Keyword/Keyword.gd
+++ Golisano/Keyword/Keyword.gd
@@ -7,8 +7,12 @@ export var backgroundColor = Color(0.0, 0.0, 0.0)
 onready var background = $ColorRect
 onready var label = $Label
 
+var initial_position
+
 func _ready():
 	var scaled_size = collider_size * Properties.TILE_SIZE
+	
+	initial_position = position
 	
 	background._set_size(scaled_size)
 	background.set_frame_color(backgroundColor)
@@ -17,3 +21,5 @@ func _ready():
 	label._set_size(scaled_size / label.get_scale())
 	label.add_color_override("font_color", labelColor)
 	
+func reset_position():
+	set_position(initial_position)
