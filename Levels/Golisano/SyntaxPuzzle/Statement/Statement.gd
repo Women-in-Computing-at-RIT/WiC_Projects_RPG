42c278d18d32c7df76e95e536d378c59749978ff
Exokem <ex010@live.com>
2024-11-20 16:23:21 -0500

    Add syntax underline for wrong values

--- Levels/Golisano/SyntaxPuzzle/Statement/Statement.gd
+++ Levels/Golisano/SyntaxPuzzle/Statement/Statement.gd
@@ -39,23 +39,35 @@ func _ready():
 	
 	# Calculate the total horizontal tiles
 	for token in tokens:
-		tiles_wide += token.collider_size.X
+		tiles_wide += token.collider_size.x
 		
-	guide_rect._set_size(Vector2(tiles_wide * Grid.TILE_SIZE, Grid.TILE_SIZE))
-	
 	# Create a rect to represent the space to fill with tokens
-	pass
+	guide_rect._set_size(Vector2(tiles_wide * Grid.TILE_SIZE, Grid.TILE_SIZE))
 
 # Retrieve the string produced by concatenating all tokens that are
 # within the statement space
 func get_text():
 	return ''
 
+func is_token_in_space(token):
+	if token.position.y != 0:
+		return false
+	if token.position.x < 0 or tiles_wide * Grid.TILE_SIZE < token.position.x:
+		return false
+		
+	return true
+
 # warning-ignore:unused_argument
 # warning-ignore:unused_argument
 func _process(delta):
 	# Iterate through all child tokens
 	# Skip tokens that do not overlap with the statement space
+	for token in tokens:
+		if not is_token_in_space(token):
+			token.set_correct(false)
+			continue
+		token.set_correct(true)
+		
 	# Sort by ascending X position
 	# Check each token against the expected tokens
 	# When one is out of place, recolor the rest to red and stop
--- Levels/Golisano/SyntaxPuzzle/Token/Token.gd
+++ Levels/Golisano/SyntaxPuzzle/Token/Token.gd
@@ -14,15 +14,19 @@ export var text = "a" setget _on_text_changed
 
 onready var label = $Label
 onready var background = $Background
+onready var underline = $Underline
 
 # Position of the token object when the scene started
 var initial_position
 
-func _on_text_color_changed(newColor):
+func set_text_color(newColor):
 	textColor = newColor
 	if label:
 		label.remove_color_override("font_color")
 		label.add_color_override("font_color", textColor)
+
+func _on_text_color_changed(newColor):
+	set_text_color(newColor)
 	update()
 	property_list_changed_notify()
 
@@ -46,6 +50,13 @@ func _ready():
 	label.set_text(text)
 	label.add_color_override("font_color", textColor)
 	
+	var underlineText = ''
+	
+	for i in range(0, text.length()):
+		underlineText += '~'
+	
+	underline.set_text(underlineText)
+	
 	# Store the initial position so we can put it back where it came
 	# from if needed
 	initial_position = position
@@ -64,7 +75,13 @@ func resize_elements(newSize):
 		label._set_size(scaled_size / label.get_scale())
 	if background:
 		background._set_size(scaled_size)
+	if underline:
+		underline._set_size(scaled_size / underline.get_scale())
 
 # Restore the token to its original position
 func reset_position():
 	set_position(initial_position)
+	
+func set_correct(is_correct):
+	underline.set_visible(not is_correct)
+
